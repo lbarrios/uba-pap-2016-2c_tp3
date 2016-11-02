5592c59b89ee9cfdf0f2fbfafdd04291a5de6c20
Martin Caravario <mcaravario@dc.uba.ar>
2016-11-02 13:06:43 -0300

    Elimino comentarios de debug

--- ej1/ej1.py
+++ ej1/ej1.py
@@ -2,33 +2,25 @@
 # -*- coding: utf-8 -*-
 
 def generar_tabla_de_bordes(text):
-	#print("Voy a buscar los bordes de %s"%text)
 	bordes = [-1]*(len(text)+1) # O(|text|)
 	for i in range(1,len(bordes)): # O(|text|)
 		j = bordes[i-1]
-		#print("i=%s, j=%s"%(i,j))
 		while(j>=0 and text[i-1] != text[j]):
-			#print("\t\twhile j=%s, text[i-1]=%s, text[j]=%s"%(j,text[i-1], text[j]))
-			#print("\t\t j = %s, bordes[j]=%s"%(j,bordes[j]))
 			j = bordes[j]
-			#print("\t\t j = %s"%j)
 		bordes[i] = j+1
-		#print("\tbordes[%s]=%s"%(i,bordes[i]))
-		#print("bordes=%s"%bordes)
 	return bordes
 
 def es_substring(nombre, apodo):
 	if(len(apodo) > len(nombre)):
 		return False
 	tabla_de_bordes = generar_tabla_de_bordes(apodo+nombre) # O(|nombre|+|apodo|) = O(n)
-	#print(tabla_de_bordes)
 	largo_buscado = len(apodo) 		# O(|apodo|) = O(n)
 	for i in tabla_de_bordes: 		# O(n)
 		if i >= largo_buscado: 		# O(1)
 			return True 			# O(1)
 	return False 					# O(1)
 
-palabra = input(); #print(palabra)
-apodo = input(); #print(apodo)
+palabra = input();
+apodo = input();
 
 print("S" if es_substring(palabra, apodo) else "N")
